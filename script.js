e53993294274326e583b13eec3e337dca266a0b8
audksamu <61708040+audksamu@users.noreply.github.com>
2020-06-07 12:59:25 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+// question 1
+var petArray = ["cat", "cow", "dog"];
+
+// question 2
+console.log(petArray[1]);
+
+// question 3
+petArray.push("sheep");
+
+// question 4
+console.log(petArray.length);
+
+// question 5
+var catObject  = {
+    name: "Ludo",
+    colour: "black",
+    age: 2
+};
+
+// question 6
+console.log(catObject.age);
+
+// question 7
+var catArray = [catObject];
+
+// question 8
+for (var i = 0; i < catArray.length; i++) {
+    console.log(catArray[i]);
+}
+
+// question 9
+function logToConsole(inputTxt) {
+    console.log(inputTxt);
+}
+
+// question 10
+logToConsole("donkey");
